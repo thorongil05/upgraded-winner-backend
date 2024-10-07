3fc4421e02bf87b8f9f0683797d8e0349f1331d5
thorongil05 <officialantonio@outlook.com>
2024-10-07 19:02:45 +0200

    test: remove useless integration tests

--- src/test/java/it/upgraded/winner/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package it.upgraded.winner;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
